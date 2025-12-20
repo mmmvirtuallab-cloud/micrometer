8388c7559ac72ee84fc26560e37f6c15a4382808
mmmvirtuallab-cloud <mmm.virtual.lab@gmail.com>
2025-12-20 21:33:56 +0530

    Initial commit

---
+++ js/intro_data.js
@@ -0,0 +1,29 @@
+// js/intro_data.js
+
+export const INTRO_CONTENT = {
+    // Top Blue Bar Text
+    headerTitle: "Micrometer",
+    
+    // Button Text & Link
+    buttonLabel: "Go to Experiment",
+    buttonLink: "micrometer.html",
+
+    // Main Page Content
+    mainHeading: "Study of Micrometer",
+    
+    // Aim Section
+    aimTitle: "Aim",
+    aimContent: "To study the working principle of a micrometer and to measure the dimensions of given components accurately.",
+
+    // Acknowledgement Section
+    ackTitle: "Acknowledgement",
+    ackContent: "The Department of Production Technology appreciates the following students for their support in the development of Metrology and measurements virtual laboratory experiment.",
+    
+    // Student List
+    students: [
+        "Mothinath G - 2023507009",
+        "Madusudarsanan J-2023507021",
+        "Prabakaran K -2023507302",
+        "Venugopal R-2023507307"
+    ]
+};
